c6cea7aa068f8041fc4001187d4b3f1cb210aeab
Chaosjeck11 <goergen-benedikt@web.de>
2026-06-10 12:59:18 +0200

    fixed .shs

--- build.sh
+++ build.sh
@@ -256,7 +256,7 @@ read -r DEPLOY_ANSWER
 if [ "$DEPLOY_ANSWER" = "j" ] || [ "$DEPLOY_ANSWER" = "J" ]; then
   echo "=== DEPLOY v$VERSION → $SERVER_HOST ==="
 
-  ssh "$SERVER_USER@$SERVER_HOST" "sudo mkdir -p $SERVER_PATH/$VERSION/linux $SERVER_PATH/$VERSION/windows $SERVER_PATH/$VERSION/android && sudo chown -R $SERVER_USER $SERVER_PATH/$VERSION"
+  ssh "$SERVER_USER@$SERVER_HOST" "mkdir -p $SERVER_PATH/$VERSION/linux $SERVER_PATH/$VERSION/windows $SERVER_PATH/$VERSION/android"
   scp -r "$OUT_DIR/"* "$SERVER_USER@$SERVER_HOST:$SERVER_PATH/$VERSION/"
   scp "$SCRIPT_DIR/Builds/latest.json" "$SERVER_USER@$SERVER_HOST:$SERVER_PATH/latest.json"
 
